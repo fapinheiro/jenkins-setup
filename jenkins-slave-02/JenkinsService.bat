1cbdd14e9af8fdae7ec55b45cf21a903ed80c3f7
FILIPE ALVES PINHEIRO <filipe.pinheiro@servexternos.isban.pt>
2020-06-25 12:22:09 +0100

    Fix jenkins slave 2 dir

--- jenkins-slave-02/JenkinsService.bat
+++ jenkins-slave-02/JenkinsService.bat
@@ -2,7 +2,7 @@
 ECHO Starting Jenkins Slave 02...
 
 SET JENKINS_SLAVE_NAME=win10-local-jnlp-02
-SET JENKINS_SLAVE_DIR="C:\Applics\Jenkins\JenkinsSlave"
+SET JENKINS_SLAVE_DIR="C:\Applics\Jenkins\JenkinsSlave02"
 SET JENKINS_HOST=localhost
 SET JENKINS_SECRET=df245d55f4188254e7b7bb205f8bcb966a7c02c662a84c9d0f38d183ceca73ad
 
