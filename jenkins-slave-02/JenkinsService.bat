ede844e28a3f027b0838f31d16eedc48e39ab890
FILIPE ALVES PINHEIRO <filipe.pinheiro@servexternos.isban.pt>
2020-06-29 15:53:45 +0100

    Set jenkins slave 2 variables

--- jenkins-slave-02/JenkinsService.bat
+++ jenkins-slave-02/JenkinsService.bat
@@ -3,7 +3,8 @@ ECHO Starting Jenkins Slave 02...
 
 SET JENKINS_SLAVE_NAME=win10-local-jnlp-02
 SET JENKINS_SLAVE_DIR="C:\Applics\Jenkins\JenkinsSlave02"
-SET JENKINS_HOST=192.168.8.103
+SET JENKINS_MASTER_HOST=192.168.8.103
+SET JENKINS_MASTER_PORT=8080
 SET JENKINS_SECRET=df245d55f4188254e7b7bb205f8bcb966a7c02c662a84c9d0f38d183ceca73ad
 
-START java -jar agent.jar -jnlpUrl http://%JENKINS_HOST%:8080/computer/%JENKINS_SLAVE_NAME%/slave-agent.jnlp -secret %JENKINS_SECRET% -workDir %JENKINS_SLAVE_DIR%
+START java -jar agent.jar -jnlpUrl http://%JENKINS_MASTER_HOST%:%JENKINS_MASTER_PORT%/computer/%JENKINS_SLAVE_NAME%/slave-agent.jnlp -secret %JENKINS_SECRET% -workDir %JENKINS_SLAVE_DIR%
